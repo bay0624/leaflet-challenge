0f91bf1791615e1e68ead192253425ff73ccf916
Abayomi Olujobi <bay.olu87@gmail.com>
2021-07-19 00:13:58 -0400

    Adjust map zoom

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,7 @@
 let myMap = L.map("map", {
     center: [37.7749, -122.4194],
-    zoom: 3
+    zoom: 4
 });
-
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
